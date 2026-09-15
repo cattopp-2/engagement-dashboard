38855564a83b988adcacfca36af3f974d0e412a8
cattopp-2 <cattopp@gmail.com>
2026-09-15 07:00:49 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-  if (pathname.startsWith('/login') || pathname.startsWith('/api/login')) {
-    return NextResponse.next()
-  }
-  const session = request.cookies.get('session')?.value
-  if (session !== process.env.PASSWORD) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+export function middleware() {
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 }
